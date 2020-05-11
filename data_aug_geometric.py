ced1442aa4911306519e29f07145c790621a0147
Emiliano946 <65175356+Emiliano946@users.noreply.github.com>
2020-05-11 16:24:43 +0200

    LP

---
+++ data_aug_geometric.py
@@ -0,0 +1,57 @@
+def data_aug_geometric(list_img, extension_file, destination_path):
+        
+    k = 1
+    
+    for img in tqdm(list_img) : 
+    
+        #TRASFORMAZIONI DI FLIP ORIZZONTALE
+        
+        # convert to numpy array
+        data = img_to_array(img)
+        # expand dimension to one sample
+        samples = expand_dims(data, 0)
+        # create image data augmentation generator
+        datagen = ImageDataGenerator(horizontal_flip=True)
+        # prepare iterator
+        it = datagen.flow(samples, batch_size=1, seed = 2)
+        # generate samples and plot
+        for i in range(1):
+            batch = it.next()
+            image = batch[0].astype('uint8')
+            plt.imsave(destination_path+"/file_"+str(k)+"_flipped_horiz_"+str(i)+extension_file, image)
+
+        #TRASFORMAZIONI DI FLIP VERTICALE
+        
+        # convert to numpy array
+        data = img_to_array(img)
+        # expand dimension to one sample
+        samples = expand_dims(data, 0)
+        # create image data augmentation generator
+        datagen = ImageDataGenerator(vertical_flip=True)
+        # prepare iterator
+        it = datagen.flow(samples, batch_size=1, seed = 2)
+        # generate samples and plot
+        for i in range(1):
+            batch = it.next()
+            image = batch[0].astype('uint8')
+            plt.imsave(destination_path+"/file_"+str(k)+"_flipped_vert_"+str(i)+extension_file, image)
+
+
+        #TRASFORMAZIONI DI ZOOM
+
+        # convert to numpy array
+        data = img_to_array(img)
+        # expand dimension to one sample
+        samples = expand_dims(data, 0)
+        # create image data augmentation generator
+        datagen = ImageDataGenerator(zoom_range=[0.2,1.0])
+        # prepare iterator
+        it = datagen.flow(samples, batch_size=1, seed = 2)
+        # generate samples and plot
+        for i in range(3):
+            batch = it.next()
+            image = batch[0].astype('uint8')
+            plt.imsave(destination_path+"/file_"+str(k)+"_zoomed_"+str(i)+extension_file, image)
+                
+        k = k+1
+
---
+++ data_aug_photometric.py
@@ -0,0 +1,22 @@
+def data_aug_photometric(list_img, extension_file, destination_path):
+
+    k = 1
+    
+    for img in tqdm(list_img) :     
+
+        # convert to numpy array
+        data = img_to_array(img)
+        # expand dimension to one sample
+        samples = expand_dims(data, 0)
+        # create image data augmentation generator
+        datagen = ImageDataGenerator(brightness_range=[0.2,1.0])
+        # prepare iterator
+        it = datagen.flow(samples, batch_size=1, seed = 2)
+        # generate samples and plot
+        for i in range(3):
+            batch = it.next()
+            image = batch[0].astype('uint8')
+            plt.imsave(destination_path+"/file_"+str(k)+"_brightnessed_"+str(i)+extension_file, image)
+
+        k = k+1
+
